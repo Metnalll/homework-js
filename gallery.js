e34ae306979cb65f74eebf33212a9db4e7ef8574
Metnalll <114822855+Metnalll@users.noreply.github.com>
2024-01-09 01:44:33 +0100

    first commit

---
+++ gallery.js
@@ -0,0 +1,52 @@
+/*Name this external file gallery.js*/
+
+function upDate(previewPic){
+ /* In this function you should 
+    1) change the url for the background image of the div with the id = "image" 
+    to the source file of the preview image*/
+
+    
+    // /*2) Change the text  of the div with the id = "image" 
+    // to the alt text of the preview image 
+
+    // */
+    document.getElementById("image").style.backgroundImage = "url('"+previewPic.src +"')";
+    document.getElementById("image").innerHTML = previewPic.alt;
+    console.log("Image Focused");
+    
+  
+	}
+
+	function unDo(){
+     /* In this function you should 
+    1) Update the url for the background image of the div with the id = "image" 
+    back to the orginal-image.  You can use the css code to see what that original URL was
+    
+    2) Change the text  of the div with the id = "image" 
+    back to the original text.  You can use the html code to see what that original text was
+    */   
+	document.getElementById('image').style.backgroundImage = "none";
+    document.getElementById('image').innerHTML = "Hover over an image below to display here";
+    console.log("Image not focused ");	
+	}
+
+
+    var previews = document.getElementsByClassName("preview");
+
+// Fügen Sie jedem Element in der Liste einen 'focus'-Event-Listener hinzu
+window.addEventListener("load", function() {
+    // Wählen Sie alle Bilder mit der Klasse 'preview' aus
+    var images = document.querySelectorAll('img.preview');
+    
+    // Fügen Sie jedem Bild einen 'focus' und 'blur' Event-Listener hinzu
+    images.forEach(function(image) {
+        image.addEventListener('focus', function() {
+            // Rufen Sie die Funktion upDate auf, wenn das Bild fokussiert wird
+            upDate(image);
+        });
+        image.addEventListener('blur', function() {
+            // Rufen Sie die Funktion unDo auf, wenn der Fokus vom Bild entfernt wird
+            unDo();
+        });
+    });
+});
